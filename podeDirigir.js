38e953e5032e8402c9b7c7237c2bf3128a5190b5
Raylan Araujo <raylanra@gmail.com>
2025-10-25 16:14:40 -0300

    cirado o programa podeDirigir.js

---
+++ podeDirigir.js
@@ -0,0 +1,9 @@
+let idade = 18;
+let possuiCarteira = true;
+let podeDirigir = idade >=18 && possuiCarteira;
+
+if (podeDirigir){
+    console.log("Pode dirigir.")
+} else {
+    console.log("Não pode dirigir.")
+}
