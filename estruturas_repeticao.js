f179aeab6eee9d85ef7f78150a30f54a1fceff32
Raylan Araujo <raylanra@gmail.com>
2025-11-02 06:53:58 -0300

    criado estrutura_repeticoes

---
+++ estruturas_repeticao.js
@@ -0,0 +1,18 @@
+let cc = 0;
+
+//estrutura while
+while (cc <= 100) {
+        console.log("Contador: ",cc);
+        cc++;
+    }
+ 
+//estrutura do-while
+do {
+    console.log("Contador: ", cc);
+    cc++;
+} while (cc <= 100);
+
+//estrutura for
+for (let cc = 0; cc <= 100; cc++) {
+    console.log("Contador: ", cc);
+}
