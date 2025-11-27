801b800f2655c64dd2ad3b4865f85e365af0615e
Raylan Araujo <raylanra@gmail.com>
2025-11-27 18:43:07 -0300

    Create percurso2.js

---
+++ percurso2.js
@@ -0,0 +1,81 @@
+//programa que soma os numeros de 1 a 100
+let soma = 0;
+for (let i = 1; i <= 100; i++){
+    soma +=i;
+}
+console.log(soma);
+
+
+
+
+
+
+// const prompt = require('prompt-sync')();
+
+// // do {
+// //     let numero = parseInt(prompt("Digite um número: "));
+// // } while (numero > 0);
+
+// let continuar = "S";
+
+// while (continuar.toUpperCase() == "S") { // Aplica .toUpperCase() à variável no teste
+//     // Opcionalmente, pode ser útil armazenar a resposta já em maiúscula
+//     let resposta = prompt("Quer continuar [ S / N ] ? ");
+//     continuar = resposta.toUpperCase();
+    
+// }
+
+// console.log("Fim do programa");
+
+
+
+// const prompt = require ('prompt-sync')();
+
+// let mes = parseInt(prompt("Digite o número do mes: "));
+
+// let nomeMes;
+
+// switch (mes){
+//     case 1:
+//         nomeMes = "Janeiro";
+//         break;
+//     case 2:
+//         nomeMes = "Fereiro";
+//         break;
+//     case 3:
+//         nomeMes = "Março";
+//         break;
+//     case 4:
+//         nomeMes = "Abril";
+//         break;
+//     case 5:
+//         nomeMes = "Maio";
+//         break;
+//     case 6:
+//         nomeMes = "Junho";
+//         break;
+//     case 7:
+//         nomeMes = "Julho";
+//         break;
+//     case 8:
+//         nomeMes = "Agosto";
+//         break;
+//     case 9:
+//         nomeMes = "Setembro";
+//         break;
+//     case 10:
+//         nomeMes = "Outubro";
+//         break;
+//     case 11:
+//         nomeMes = "Novembro";
+//         break;
+//     case 12:
+//         nomeMes = "Dezembro";
+//         break;
+//     default:
+//         nomeMes = "Mês inválido";
+//         break;
+// }
+
+// console.log(nomeMes);
+
